2ac57ac2a9d521a3c12db4990f72a5c82b2ef9f6
gitcryds <COR2047148@maricopa.edu>
2025-03-24 17:06:57 -0700

    Commit 2

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-print("Hello World!")
-print("Bye World!")
+"""
+Corey Rydstrom
+Module 08 Lab Assignment
+Part B
+
+The first file in the repo with a print statement.
+"""
+
+
+print("This is the main file.")
---
+++ second_file.py
@@ -0,0 +1,9 @@
+"""
+Corey Rydstrom
+Module 08 Lab Assignment
+Part B
+
+The second file in the repo with a print statement.
+"""
+
+print("This is the second Python file.")
